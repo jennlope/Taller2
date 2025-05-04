1c079934982596b0bd0117307c40bfb38c4f2a2f
Jennifer <lopezgomezjennifer50@hotmail.com>
2025-05-04 14:11:21 -0500

    Primera fase Pokeneas

---
+++ app/data.py
@@ -0,0 +1,82 @@
+pokeneas = [
+    {
+        "id": 1,
+        "nombre": "Andiochu",
+        "altura": 1.2,
+        "habilidad": "Café explosivo",
+        "imagen": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
+        "frase": "La arepa es circular como la vida misma."
+    },
+    {
+        "id": 2,
+        "nombre": "Medejin",
+        "altura": 0.9,
+        "habilidad": "Transmi-rayo",
+        "imagen": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
+        "frase": "Solo quien ha madrugado conoce el poder del tinto."
+    },
+    {
+        "id": 3,
+        "nombre": "Silleterin",
+        "altura": 1.0,
+        "habilidad": "Carga floral",
+        "imagen": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+        "frase": "Llevar con orgullo las raíces es volar sin alas."
+    },
+    {
+        "id": 4,
+        "nombre": "Guarapan",
+        "altura": 0.7,
+        "habilidad": "Espuma dulce",
+        "imagen": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
+        "frase": "Más sabio es el que fermenta paciencia que el que estalla en rabia."
+    },
+    {
+        "id": 5,
+        "nombre": "Bandejin",
+        "altura": 1.5,
+        "habilidad": "Frijolazo solar",
+        "imagen": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/143.png",
+        "frase": "Con hambre no hay sabiduría que aguante."
+    },
+    {
+        "id": 6,
+        "nombre": "Tamalor",
+        "altura": 0.6,
+        "habilidad": "Amarre ancestral",
+        "imagen": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/052.png",
+        "frase": "A veces, lo más envuelto contiene lo más sabroso."
+    },
+    {
+        "id": 7,
+        "nombre": "Aguapanelo",
+        "altura": 1.1,
+        "habilidad": "Dulzura revividora",
+        "imagen": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/054.png",
+        "frase": "Nada que un aguapanela caliente no cure."
+    },
+    {
+        "id": 8,
+        "nombre": "Metroplin",
+        "altura": 1.3,
+        "habilidad": "Ruta veloz",
+        "imagen": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/026.png",
+        "frase": "En el viaje constante también se encuentra la calma."
+    },
+    {
+        "id": 9,
+        "nombre": "Arepon",
+        "altura": 0.8,
+        "habilidad": "Croqueta solar",
+        "imagen": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/133.png",
+        "frase": "Toda idea necesita su capa de maíz."
+    },
+    {
+        "id": 10,
+        "nombre": "Envigador",
+        "altura": 1.4,
+        "habilidad": "Gol de esquina",
+        "imagen": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/095.png",
+        "frase": "Hasta en la última jugada puede venir la victoria."
+    }
+]
---
+++ app/routes.py
@@ -0,0 +1,28 @@
+from flask import Blueprint, jsonify, render_template_string
+import random
+from .data import pokeneas
+from .utils import get_container_id
+
+main = Blueprint('main', __name__)
+
+@main.route('/pokenea/json')
+def pokenea_json():
+    p = random.choice(pokeneas)
+    return jsonify({
+        "id": p["id"],
+        "nombre": p["nombre"],
+        "altura": p["altura"],
+        "habilidad": p["habilidad"],
+        "contenedor": get_container_id()
+    })
+
+@main.route('/pokenea/view')
+def pokenea_view():
+    p = random.choice(pokeneas)
+    html = f"""
+    <h1>{p['nombre']}</h1>
+    <img src="{p['imagen']}" alt="Imagen de {p['nombre']}" width="300"><br>
+    <p><em>{p['frase']}</em></p>
+    <p>ID del contenedor: <strong>{get_container_id()}</strong></p>
+    """
+    return render_template_string(html)
